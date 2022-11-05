f329a4db8dd63877090631691f70b30da4aef0b2
vdrozdik <drozdikv90@gmail.com>
2022-11-05 17:28:40 +0200

    add select statements for channel

--- app/cmd/sync/main.go
+++ app/cmd/sync/main.go
@@ -14,25 +14,40 @@ var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 func main() {
 	wg := &sync.WaitGroup{}
 	m := &sync.RWMutex{}
+	cacheCh := make(chan book.Book)
+	dbCh := make(chan book.Book)
 	for i := 0; i < 10; i++ {
 		id := rnd.Intn(10) + 1
 		wg.Add(2)
-		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex, ch chan<- book.Book) {
 			if b, ok := queryCache(id, m); ok {
-				fmt.Println("from cache")
-				fmt.Println(b)
+				ch <- b
 			}
 			wg.Done()
-		}(id, wg, m)
+		}(id, wg, m, cacheCh)
 
-		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex, ch chan<- book.Book) {
 			if b, ok := queryDatabase(id, m); ok {
+				m.Lock()
+				cache[id] = b
+				m.Unlock()
+				ch <- b
+			}
+			wg.Done()
+		}(id, wg, m, dbCh)
+		go func(chacheCh, dbCh <-chan book.Book) {
+			select {
+			case b := <-chacheCh:
+				fmt.Println("from chache")
+				fmt.Println(b)
+				<-dbCh
+			case b := <-dbCh:
 				fmt.Println("from database")
 				fmt.Println(b)
 			}
-			wg.Done()
-		}(id, wg, m)
 
+		}(cacheCh, dbCh)
+		time.Sleep(150 * time.Millisecond)
 		fmt.Printf("book not found with id")
 	}
 	wg.Wait()
@@ -49,9 +64,6 @@ func queryDatabase(id int, m *sync.RWMutex) (book.Book, bool) {
 	time.Sleep(100 * time.Millisecond)
 	for _, b := range book.Books {
 		if b.ID == id {
-			m.Lock()
-			cache[id] = b
-			m.Unlock()
 			return b, true
 		}
 	}
